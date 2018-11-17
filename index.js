1de0c5008e05b0341c388cb93845f8ab0f87be29
lykmapipo <lallyelias87@gmail.com>
2018-11-17 09:06:56 +0300

    Implement Item Model - Add code, color and icon into item class diagram - Add Item schema definion - Setup package test environment - Setup item model unit spec - Setup item schema unit spec

---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+
+/**
+ * @name emis-resource
+ * @module emis-resource
+ * @description WIP
+ *
+ * @see {@link https://en.wikipedia.org/wiki/Disaster}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @license MIT
+ * @since 1.0.0
+ * @version 0.1.0
+ * @public
+ *
+ * @example
+ *
+ * const { app } = require('@codetanzania/emis-resource');
+ * app.start(error => { ... });
+ *
+ */
+
+
+/* dependencies */
+const path = require('path');
+const _ = require('lodash');
+const mongoose = require('mongoose');
+require('mongoose-schema-jsonschema')(mongoose);
+const app = require('@lykmapipo/express-common');
+
+
+/* declarations */
+const pkg = require(path.join(__dirname, 'package.json'));
+const fields = [
+  'name',
+  'description',
+  'version',
+  'license',
+  'homepage',
+  'repository',
+  'bugs',
+  'sandbox',
+  'contributors'
+];
+
+
+/* extract information from package.json */
+exports.info = _.merge({}, _.pick(pkg, fields));
+
+
+/* export models */
+exports.Item = require(path.join(__dirname, 'lib', 'item.model'));
+
+
+/* export routers*/
+// exports.itemRouter = require(path.join(__dirname, 'lib', 'item.http.router'));
+
+
+/* export router api version */
+// exports.apiVersion = itemRouter.apiVersion;
+
+
+/* export app */
+Object.defineProperty(exports, 'app', {
+  get() {
+
+    //TODO bind oauth middlewares authenticate, token, authorize
+
+    /* bind incident type router */
+    // app.mount(itemRouter);
+    return app;
+  }
+
+});
---
+++ test/unit/item.model.spec.js
@@ -0,0 +1,90 @@
+'use strict';
+
+
+/* dependencies */
+const path = require('path');
+const _ = require('lodash');
+const { expect } = require('chai');
+const { Item } = require(path.join(__dirname, '..', '..'));
+
+describe('Item Instance', () => {
+
+  it('`preValidate` should be a function', () => {
+    const item = Item.fake();
+    expect(item.preValidate).to.exist;
+    expect(item.preValidate).to.be.a('function');
+    expect(item.preValidate.length).to.be.equal(1);
+    expect(item.preValidate.name).to.be.equal('preValidate');
+  });
+
+  it('`preValidate` should set code', (done) => {
+    const item = Item.fake();
+    item.code = undefined;
+    expect(item.code).to.not.exist;
+
+    item.preValidate((error) => {
+      expect(item.code).to.exist;
+      done(error);
+    });
+  });
+
+  it('`preValidate` should set color', (done) => {
+    const item = Item.fake();
+    item.color = undefined;
+    expect(item.color).to.not.exist;
+
+    item.preValidate((error) => {
+      expect(item.color).to.exist;
+      done(error);
+    });
+  });
+
+});
+
+describe('Item Statics', () => {
+
+  it('should expose model name', () => {
+    expect(Item.MODEL_NAME).to.exist;
+    expect(Item.MODEL_NAME).to.be.equal('Item');
+  });
+
+  it('should expose collection name', () => {
+    expect(Item.COLLECTION_NAME).to.exist;
+    expect(Item.COLLECTION_NAME).to.be.equal('items');
+  });
+
+  it('should expose autopulate options', () => {
+    expect(Item.OPTION_AUTOPOPULATE).to.exist;
+    expect(Item.OPTION_AUTOPOPULATE)
+      .to.be.eql({
+        maxDepth: Item.POPULATION_MAX_DEPTH
+      });
+  });
+
+  it('should expose item types', () => {
+    expect(Item.DEFAULT_TYPE).to.exist;
+    expect(Item.DEFAULT_TYPE).to.be.equal('Other');
+
+    expect(Item.TYPES).to.exist;
+    expect(Item.TYPES).to.be.eql(_.orderBy(_.map([
+      'Equipment', 'Consumable',
+      'Vehicle', 'Service',
+      'Other'
+    ], _.startCase)));
+  });
+
+  it('should expose item unit of measures', () => {
+    expect(Item.DEFAULT_UOM).to.exist;
+    expect(Item.DEFAULT_UOM).to.be.equal('unit');
+
+    expect(Item.UOMS).to.exist;
+    expect(Item.UOMS).to.be.eql(_.orderBy(_.map([
+      'bag', 'box', 'carton',
+      'drum', 'dozen', 'gallon',
+      'kit', 'pallet', 'unit',
+      'piece', 'bucket', 'bottle',
+      'crate'
+    ], _.toLower)));
+  });
+
+});
