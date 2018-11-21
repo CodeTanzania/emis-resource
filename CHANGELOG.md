2c03965c0636e72490597dd6a1b4475631abf19d
lykmapipo <lallyelias87@gmail.com>
2018-11-21 17:38:11 +0300

    Prepare for v1.0.0 release - Fix typos - Build API doc - Prepare release changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,4 @@
-
+# 1.0.0 / 21-11-2018
+- Implement initial item, stock, adjustment data model
+- Implement item, stock and ajustment http routers
+- Build latest apidoc
---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "emis-resource",
+  "version": "1.0.0",
+  "description": "A representation of item(i.e materials, services, staff, assets etc.) and it stock that may be consumed or made available in emergency(or disaster) management(or event).",
+  "title": "EMIS - Resource",
+  "url": "https://emis-resource.herokuapp.com/v1",
+  "sampleUrl": "https://emis-resource.herokuapp.com/v1",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-11-21T14:35:33.006Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
