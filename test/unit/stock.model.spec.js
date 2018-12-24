9ca463929e6a8d577371d9f9a9c1a6692bbe64ce
lykmapipo <lallyelias87@gmail.com>
2018-12-24 13:43:01 +0300

    Refactor and improve unit specs readability

--- test/unit/stock.model.spec.js
+++ test/unit/stock.model.spec.js
@@ -2,9 +2,9 @@
 
 
 /* dependencies */
-const path = require('path');
 const { expect } = require('chai');
-const { Stock } = require(path.join(__dirname, '..', '..'));
+const { include } = require('@lykmapipo/include');
+const { Stock } = include(__dirname, '..', '..');
 
 describe('Stock Instance', () => {
 
